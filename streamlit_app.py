bdf726053b2af47f5904619a807a65ae10560379
Kaushalya-Dananjani <kaushalyaaththanayaka98@gmail.com>
2026-05-14 10:00:11 +0530

    Update streamlit_app.py
    update session

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
+
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
@@ -48,3 +48,6 @@ if time_to_insert:
         
     else:
         st.warning("Please select at least one ingredient before submitting.")
+
+cnx = st.connection("snowflake")
+session = cnx.session()
